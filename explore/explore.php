474f4164c31ced17f4912736d6054c0e13b638a0
Ishu Agrawal <ishu.agrawal@gmail.com>
2022-05-26 04:34:13 -0700

    Fixed jquery import

--- explore/explore.php
+++ explore/explore.php
@@ -43,12 +43,7 @@
 		</div>
     </div>
     
-    <!-- Import jquery library -->
-    <script
-    src="http://code.jquery.com/jquery-3.5.1.min.js"
-    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-    crossorigin="anonymous">
-    </script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <!-- Add external JS file -->
     <script src="explore.js"></script>
