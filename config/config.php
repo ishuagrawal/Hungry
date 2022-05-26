e97e384d55958151d63d82cd84771cb718d10f1d
Ishu Agrawal <ishu.agrawal@gmail.com>
2022-05-26 04:21:29 -0700

    Added heroku database config

--- config/config.php
+++ config/config.php
@@ -2,9 +2,15 @@
 
 session_start(); // for every page that requires the config file, we will also start session for that page
 
-define('DB_HOST', '303.itpwebdev.com');
-define('DB_USER', 'ishuagra_db_user');
-define('DB_PASS', 'uscItp2021!');
-define('DB_NAME', 'ishuagra_recipes');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+define('DB_HOST', $server);
+define('DB_USER', $username);
+define('DB_PASS', $password);
+define('DB_NAME', $db);
 
 ?>
